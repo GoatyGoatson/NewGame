f5fb776fb76c2c2842d5ec629da4d06d2f27ed07
GoatyGoatson <davidjbeyera@web.de>
2024-12-03 00:05:04 +0100

    major claude changes

--- static/js/everything.js
+++ static/js/everything.js
@@ -1,10 +1,17 @@
-// Firebase setup
-import { initializeApp } from "firebase/app";
-import { getDatabase, ref, set, onValue, update } from "firebase/database";
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.18.0/firebase-app.js';
+import { getDatabase, ref, set, update, push, onValue, remove } from 'https://www.gstatic.com/firebasejs/9.18.0/firebase-database.js';
 
 const firebaseConfig = {
-    // Your Firebase config here
+  apiKey: "AIzaSyBXvE64zxiLq4llMRX-sG8oMC5NZ-n1lBw",
+  authDomain: "bulletbound-70a04.firebaseapp.com",
+  databaseURL: "https://bulletbound-70a04-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "bulletbound-70a04",
+  storageBucket: "bulletbound-70a04.firebasestorage.app",
+  messagingSenderId: "512551082564",
+  appId: "1:512551082564:web:eeded9d53aba74e2f0ba11",
+  measurementId: "G-0KBGW2TCQS"
 };
+
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
